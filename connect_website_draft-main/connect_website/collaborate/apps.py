3256f0b8c30f7358bc1680e43b48d6cf58eb734d
MsTuby <mricha018@gmail.com>
2024-10-27 09:28:13 +0530

    Delete connect_website_draft-main/connect_website directory

--- connect_website_draft-main/connect_website/collaborate/apps.py
+++
@@ -1,6 +0,0 @@
-# C:\Users\Admin\connect_website\collaborate\apps.py
-from django.apps import AppConfig
-
-class CollaborateConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'collaborate'
--- connect_website_draft-main/connect_website/collaborate/tests.py
+++
@@ -1,8 +0,0 @@
-# C:\Users\Admin\connect_website\collaborate\tests.py
-from django.test import TestCase
-from .models import Idea
-
-class IdeaModelTest(TestCase):
-    def test_string_representation(self):
-        idea = Idea(title="Test Idea")
-        self.assertEqual(str(idea), "Test Idea")
